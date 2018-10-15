2fc85c376bd33b55ca0e09322a85a070f4858c8c
joyceliaoo <jliao@stuy.edu>
2018-10-15 11:01:57 -0400

    functions

---
+++ library.h
@@ -0,0 +1,8 @@
+void add_song(char *song, char *musician);
+struct song_node *search_song(char *song, char *musician);
+struct song_node *search_artist(char *musician);
+void print_alpha(char letter);
+void print_artist(char *musician);
+void print_all();
+void shuffle();
+
---
+++ song.h
@@ -0,0 +1,15 @@
+struct song_node{ 
+  char name[100];
+  char artist[100];
+  struct song_node *next;
+};
+
+void print_song(struct song_node *target);
+struct song_node *insert_front(struct song_node *list, char *song, char *musician);
+struct song_node *insert_alpha(struct song_node *list, char *song, char *musician);
+struct song_node *find_one(struct song_node *list, char *song, char *musician);
+struct song_node *find_first(struct song_node *list, char *musician);
+struct song_node *random(struct song_node *list);
+struct song_node *remove(struct song_node *list, char *song);
+struct song_node *free_list(struct song_node *list);
+
